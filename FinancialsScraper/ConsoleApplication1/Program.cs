d77a5891213a27e2d90020d69ed9d84a07b7eb38
zachhodges <zach.hodges@ao.com>
2021-01-10 23:06:01 +0000

    wip

---
+++ FinancialsScraper/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            
+            var address = "https://www.wsj.com/market-data/quotes/TSM/financials";
+
+            var context = BrowsingContext.New(config);
+            
+            var document = await context.OpenAsync(address);
+            
+            var perShareDataTable = document.QuerySelector("table[class='cr_dataTable cr_mod_pershare']");
+        }
+    }
+}
--- FinancialsScraper/FinancialsScraper/Function.cs
+++ FinancialsScraper/FinancialsScraper/Function.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using AngleSharp;
+using AngleSharp.Dom;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -12,16 +14,30 @@ namespace FinancialsScraper
 {
     public class Function
     {
-        
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async Task<string> FunctionHandler(string input, ILambdaContext context)
         {
-            return input?.ToUpper();
+           
+            var config = Configuration.Default.WithDefaultLoader();
+            
+            var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
+            
+            var browsingContext = BrowsingContext.New(config);
+            
+            var document = await browsingContext.OpenAsync(address);
+            
+            var cellSelector = "tr.vevent td:nth-child(3)";
+            
+            var cells = document.QuerySelectorAll(cellSelector);
+            
+            var titles = cells.Select(m => m.TextContent);
+
+            return null;
         }
     }
 }

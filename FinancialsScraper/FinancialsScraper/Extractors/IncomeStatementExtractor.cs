719bd4cd6301945ab08efdd349b8717ae6e7a33c
Zach Hodges <zach.hodges@ao.com>
2021-04-21 23:31:32 +0100

    Now getting income statement table

--- FinancialsScraper/FinancialsScraper/Extractors/IncomeStatementExtractor.cs
+++ FinancialsScraper/FinancialsScraper/Extractors/IncomeStatementExtractor.cs
@@ -9,19 +9,7 @@ namespace FinancialsScraper.Extractors
     {
         public IEnumerable<string> GetDataTableCells(IDocument document, string selector = "")
         {
-            // return GetDataTable(document, selector).Children.SelectMany(x => x.Children.Select(y => y.TextContent));
-
-            var allChildren = GetIncomeSatement(document).SelectMany(x => x.Children.SelectMany(y => y.Children.SelectMany(z => z.Children)));
-
-
-            var child = GetIncomeSatement(document)
-                .SelectMany(x =>
-                    x.Children
-                        .Select(y =>
-                            y.GetElementsByClassName("cr_financials_table").SelectMany(z => z.LocalName == "table");
-            
-            
-            //var table = allChildren.SelectMany(x => x.Children.Select(y => y.QuerySelector("cr_financials_table")));
+            var incomeStatementTable = GetIncomeSatement(document).SelectMany(x => x.Children.Select(y => y.GetElementsByClassName("cr_financials_table"))).SingleOrDefault(z => z.Any());
 
             return null;
         }
